195975ae72dab0ba35c251f8f9773eb5ed21f3fc
hymaybituin <hymaybituin@student.apc.edu.ph>
2025-04-13 16:47:42 +0800

    commit

--- database/migrations/2025_03_10_064939_create_products_table.php
+++ database/migrations/2025_03_10_064939_create_products_table.php
@@ -30,7 +30,6 @@ return new class extends Migration
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('updated_by')->constrained('users');
             $table->timestamps();
-            
         });
         
         
